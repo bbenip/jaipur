b868b69f908bf13c591eab33130e32ad05c132d6
Baljinder Benipal <baljinder.benipal@instacart.com>
2025-03-15 20:44:32 -0400

    Rename resource strings to be lowercase

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,24 @@ import TokenSidebar from "./components/TokenSidebar";
 
 const App = () => {
   const tokensRemaining = [
-    { name: 'Diamond', quantities: [5, 5, 5, 7, 7] },
-    { name: 'Gold', quantities: [5, 5, 5, 6, 6] },
-    { name: 'Silver', quantities: [5, 5, 5, 5, 5] },
-    { name: 'Cloth', quantities: [1, 1, 2, 2, 3, 3, 5] },
-    { name: 'Spice', quantities: [1, 1, 2, 2, 3, 3, 5] },
-    { name: 'Leather', quantities: [1, 1, 1, 1, 1, 1, 2, 3, 4] },
+    { name: 'diamond', quantities: [5, 5, 5, 7, 7] },
+    { name: 'gold', quantities: [5, 5, 5, 6, 6] },
+    { name: 'silver', quantities: [5, 5, 5, 5, 5] },
+    { name: 'cloth', quantities: [1, 1, 2, 2, 3, 3, 5] },
+    { name: 'spice', quantities: [1, 1, 2, 2, 3, 3, 5] },
+    { name: 'leather', quantities: [1, 1, 1, 1, 1, 1, 2, 3, 4] },
   ];
 
   const player1 = {
     name: 'Player 1',
     resources: [],
     tokens: [
-      { name: 'Diamond', quantities: [] },
-      { name: 'Gold', quantities: [] },
-      { name: 'Silver', quantities: [] },
-      { name: 'Cloth', quantities: [] },
-      { name: 'Spice', quantities: [] },
-      { name: 'Leather', quantities: [] },
+      { name: 'diamond', quantities: [] },
+      { name: 'gold', quantities: [] },
+      { name: 'silver', quantities: [] },
+      { name: 'cloth', quantities: [] },
+      { name: 'spice', quantities: [] },
+      { name: 'leather', quantities: [] },
     ],
   };
 
@@ -30,23 +30,23 @@ const App = () => {
     name: 'Player 2',
     resources: [],
     tokens: [
-      { name: 'Diamond', quantities: [] },
-      { name: 'Gold', quantities: [] },
-      { name: 'Silver', quantities: [] },
-      { name: 'Cloth', quantities: [] },
-      { name: 'Spice', quantities: [] },
-      { name: 'Leather', quantities: [] },
+      { name: 'diamond', quantities: [] },
+      { name: 'gold', quantities: [] },
+      { name: 'silver', quantities: [] },
+      { name: 'cloth', quantities: [] },
+      { name: 'spice', quantities: [] },
+      { name: 'leather', quantities: [] },
     ],
   };
 
   const deck = [
-    ...Array(6).fill('Diamond'),
-    ...Array(6).fill('Gold'),
-    ...Array(6).fill('Silver'),
-    ...Array(8).fill('Cloth'),
-    ...Array(8).fill('Spice'),
-    ...Array(10).fill('Leather'),
-    ...Array(11).fill('Camel'),
+    ...Array(6).fill('diamond'),
+    ...Array(6).fill('gold'),
+    ...Array(6).fill('silver'),
+    ...Array(8).fill('cloth'),
+    ...Array(8).fill('spice'),
+    ...Array(10).fill('leather'),
+    ...Array(11).fill('camel'),
   ];
 
   // Fisher-Yates shuffle
