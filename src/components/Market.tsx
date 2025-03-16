fa6719f8d3e5f1342cda72bc71d0dbca2f7c513c
Baljinder Benipal <baljinder.benipal@instacart.com>
2025-03-15 21:51:07 -0400

    Render resource cards in the market

--- src/components/Market.tsx
+++ src/components/Market.tsx
@@ -8,9 +8,19 @@ const Market = ({ market }: MarketProps) => {
   return (
     <div>
       <h2>Market</h2>
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '0.5vh' }}>
         {market.resources.map((resource, index) => (
-          <div key={index}>{resource}</div>
+          <div key={index}>
+            <img
+              src={`/src/assets/cards/${resource}_card.png`}
+              alt={resource}
+              style={{
+                borderRadius: '8%',
+                height: '18vh',
+                boxShadow: '1px 2px 1px 1px rgba(0,0,0,0.5)',
+              }}
+            />
+          </div>
         ))}
       </div>
     </div>
