757525d502288fea6f48327da17abad478cae083
Baljinder Benipal <baljinder.benipal@instacart.com>
2025-03-15 21:15:24 -0400

    Reduce token size and render with overlap

--- src/components/TokenBar.tsx
+++ src/components/TokenBar.tsx
@@ -7,12 +7,20 @@ const TokenBar = ({ tokenRow }: TokenBarProps) => {
   const tokenQuantities = tokenRow.quantities;
 
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
       {tokenQuantities.map((quantity, index) => (
-        <div key={index}>
+        <div
+          key={index}
+          style={{
+            marginLeft: index === 0 ? 0 : '-6vh',
+            position: 'relative',
+            zIndex: index,
+          }}
+        >
           <img
             src={`/src/assets/resource_tokens/${tokenName}_token_${quantity}.webp`}
             alt={`${tokenName}-${quantity}`}
+            style={{ height: '8vh', width: '8vh' }}
           />
         </div>
       ))}
