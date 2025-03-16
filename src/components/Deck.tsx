595957522ce24e4542d384d092ea47eccbe275be
Baljinder Benipal <baljinder.benipal@instacart.com>
2025-03-15 21:39:15 -0400

    Reduce card size and render backside with overlap

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -6,12 +6,25 @@ const Deck = ({ deck }: DeckProps) => (
   <div>
     <h2>Deck</h2>
 
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
       {deck.map((resource, index) => (
-        <div key={index}>
+        <div
+          key={index}
+          style={{
+            marginRight: index === 0 ? 0 : '-12.92vh',
+            marginTop: index === 0 ? 0 : `-${0.05 * index}vh`,
+            position: 'relative',
+            zIndex: index,
+          }}
+        >
           <img
-            src={`/src/assets/cards/${resource}_card.png`}
+            src={`/src/assets/cards/back_card.png`}
             alt={resource}
+            style={{
+              borderRadius: '8%',
+              height: '18vh',
+              boxShadow: '1px 2px 1px 1px rgba(0,0,0,.1)',
+            }}
           />
         </div>
       ))}
