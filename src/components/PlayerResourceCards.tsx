7aeea5c4e68bbd2f01984a3f64b7d70accf6ea89
Baljinder Benipal <baljinder.benipal@instacart.com>
2025-03-15 21:57:54 -0400

    Render player's resource cards

--- src/components/PlayerResourceCards.tsx
+++ src/components/PlayerResourceCards.tsx
@@ -1,11 +1,30 @@
-
 type PlayerResourceCardsProps = {
   resources: string[];
 };
 
 const PlayerResourceCards = ({ resources }: PlayerResourceCardsProps) => (
-  <div>
-    {resources.map((resource, index) => <div key={index}>{resource}</div>)}
+  <div style={{ display: 'flex', flexDirection: 'row' }}>
+    {resources.map((resource, index) => (
+      <div
+        key={index}
+            style={{
+              marginLeft: index === 0 ? 0 : '-9vh',
+              marginTop: index % 2 === 0 ? 0 : '-0.9vh',
+              position: 'relative',
+              zIndex: index,
+            }}
+          >
+            <img
+              src={`/src/assets/cards/${resource}_card.png`}
+              alt={resource}
+              style={{
+                borderRadius: '8%',
+              height: '18vh',
+            boxShadow: '1px 2px 1px 1px rgba(0,0,0,0.5)',
+          }}
+        />
+      </div>
+    ))}
   </div>
 );
 
